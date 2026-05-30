ac9ee45255d07671150df5f7c81d6b21c928462b
sakina-cmpn <sakinavi786@gmail.com>
2026-05-30 13:32:30 +0530

    feat:Particle_Universe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,6 +30,8 @@ import TextToResume from './pages/TextToResume';
 import About from './components/portfolio/templates/Tech_Startup/About';
 import ChatbotPortfolio from "./components/portfolio/templates/Chatbot_Portfolio";
 import GamifiedXP from "./components/portfolio/templates/Gamified_XP";
+import TelescopeZoom from "./components/portfolio/templates/Telescope_Zoom";
+import ParticleUniverse from "./components/portfolio/templates/Particle_Universe";
 
 import JobTracker from './pages/JobTracker';
 const Community = lazy(() => import('./pages/Community'));
@@ -183,7 +185,8 @@ function AppRoutes() {
         <Route path="/templates" element={<RouteErrorBoundary><TemplateGallery /></RouteErrorBoundary>} />
         <Route path="/templates/chatbot" element={<ChatbotPortfolio />} />
         <Route path="/templates/gamified-xp" element={<GamifiedXP />} />
-        
+        <Route path="/templates/telescope-zoom" element={<TelescopeZoom />} />
+        <Route path="/templates/particle-universe" element={<ParticleUniverse />} />
         {/* Core Protected Routes */}
         <Route 
   path="/dashboard" 
