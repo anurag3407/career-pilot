18a6b007e2daae3038cb6a42769e54189c04af59
codecrafted1 <khushiambastha16@gmail.comn>
2026-06-01 15:37:48 +0530

    improve backend

--- backend/src/middleware/globalErrorHandler.js
+++ backend/src/middleware/globalErrorHandler.js
@@ -2,11 +2,19 @@ export const globalErrorHandler = (err, req, res, next) => {
   console.error(err);
 
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Something went wrong";
+
+  const message =
+    err.isOperational
+      ? err.message
+      : statusCode === 500
+      ? "Internal server error"
+      : err.message || "Something went wrong";
 
   res.status(statusCode).json({
     success: false,
-    message: message
+    statusCode,
+    message,
+    details: err.details || null,
   });
 };
 
