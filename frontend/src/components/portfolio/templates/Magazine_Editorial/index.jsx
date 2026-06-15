77766ef3863e5b8a4c02bde5e0b93e739faf0cff
aditi25srivastava <aditisri372@gmail.com>
2026-06-15 20:41:33 +0530

    feat: implement Magazine Editorial About section

---
+++ frontend/src/components/portfolio/templates/Magazine_Editorial/index.jsx
@@ -0,0 +1,15 @@
+export default function MagazineEditorial() {
+  return (
+    <div
+      style={{
+        background: "red",
+        color: "white",
+        minHeight: "100vh",
+        fontSize: "40px",
+        padding: "50px",
+      }}
+    >
+      MAGAZINE EDITORIAL WORKING
+    </div>
+  );
+}
